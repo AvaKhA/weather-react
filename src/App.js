52175bab516e5571fb33b58c7e59ebe1dcfde30c
AvaKhA <109901314+AvaKhA@users.noreply.github.com>
2024-10-06 18:58:24 +0330

    Added footer & GitHub link to the project

--- src/App.js
+++ src/App.js
@@ -7,6 +7,15 @@ function App() {
     <div className="App">
       <h1>Weather App</h1>
       <Weather />
+      <footer>
+        <p>
+          This project was coded by Ava Khalili and is{" "}
+          <a href="https://github.com/AvaKhA/weather-react" target="_blank">
+            open-sourced on GitHub
+          </a>{" "}
+          and hosted on Netlify
+        </p>
+      </footer>
     </div>
   );
 }
